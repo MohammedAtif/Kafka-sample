47427197460905eed7448868603127536bc6dcd3
Mohammed Atif <atif@zemosolabs.com>
2018-12-11 15:37:12 +0530

    Docker setup completed

--- src/main/java/com/zemoso/kafkasample/configuration/RedisConfiguration.java
+++ src/main/java/com/zemoso/kafkasample/configuration/RedisConfiguration.java
@@ -6,6 +6,7 @@ import com.zemoso.kafkasample.repositories.TrendDataRepository;
 import com.zemoso.kafkasample.repositories.impl.TrendDataRedisImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -14,7 +15,8 @@ public class RedisConfiguration {
 
     @Bean
     JedisConnectionFactory getJedisConnectionFactory() {
-        return new JedisConnectionFactory();
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("redis", 6379);
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
